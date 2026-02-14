bac4655f3ad7c9f7cc968d4a9a28e3a67e25a5ca
Mrmayman <navneetkrishna22@gmail.com>
2026-02-14 14:59:06 +0530

    Unironically rename the whole project to whatsmeow-nchat to emphasise the reliance on nchat

---
+++ whatsmeow-nchat-sys/examples/hello.rs
@@ -0,0 +1,7 @@
+fn main() {
+    unsafe {
+        let r =
+            whatsmeow_nchat_sys::CWmInit(c"./prof".as_ptr() as *mut _, c"".as_ptr() as *mut _, 1);
+        println!("{r}")
+    }
+}
--- whatsmeow-sys/examples/hello.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    unsafe {
-        let r = whatsmeow_sys::CWmInit(c"./prof".as_ptr() as *mut _, c"".as_ptr() as *mut _, 1);
-        println!("{r}")
-    }
-}
